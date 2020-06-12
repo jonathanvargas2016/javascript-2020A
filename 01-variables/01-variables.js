d205a2779e9457f44c39ac69a16be16fc640dd15
Jonathan S Vargas <jonathan.vargas01@epn.edu.ec>
2020-06-12 18:31:21 -0500

    creacion de archivo.js

---
+++ 01-variables/01-variables.js
@@ -0,0 +1,14 @@
+//tipado ->es decir saber que tipo de dato es.
+//python y javascript es NO TIPADO
+
+//mutables (que pueden cambiar en el tiempo) o inmutables
+
+//declaracion de variables mutables
+var numeroUno = 1;
+let numeroDos = 2;
+
+numeroUno = 5;
+numeroDos = 8;
+
+numeroUno = false;
+numeroDos = true;
