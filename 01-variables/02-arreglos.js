50cca2a8417a345de4b54fcedec48ca67eea95b1
Jonathan S Vargas <jonathan.vargas01@epn.edu.ec>
2020-06-16 18:13:16 -0500

    definicion de arreglos

---
+++ 01-variables/02-arreglos.js
@@ -0,0 +1,13 @@
+
+const  arreglo = [1,2,3,4,5,6,7,8,9]
+
+//no se puede modificar un arreglo cuando es definido con const
+
+let cualquierCosa = 1
+cualquierCosa = true
+cualquierCosa = "hola"
+cualquierCosa = undefined
+cualquierCosa = null
+cualquierCosa = {}
+
+let arregloTodo = [1,2,3,"hola",true, undefined,{},[1,2,true]]
