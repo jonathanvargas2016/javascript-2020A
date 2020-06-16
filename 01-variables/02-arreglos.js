055ce74d297d1bb5eb584dc866fd8dd904a32f6f
Jonathan S Vargas <jonathan.vargas01@epn.edu.ec>
2020-06-16 18:00:43 -0500

    definicion  de arreglos

---
+++ 01-variables/02-arreglos.js
@@ -0,0 +1,19 @@
+
+//arreglos
+
+const arreglo =[1,2,3,4,5,6,7,8,9]
+
+let cualquierCosa = "ASD"
+cualquierCosa = true
+cualquierCosa = undefined
+cualquierCosa = null
+cualquierCosa = {}
+cualquierCosa = []
+
+console.log(cualquierCosa)
+
+const arregloTodo = [
+    true,1,2.2,"Jona",undefined,null,{},[1,2,3,true,"A"],
+]
+
+//los arreglos pueden ser solo de numeros o de casi cualquier cosa
