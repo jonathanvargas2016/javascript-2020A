f319e0cd816ef1eb01d789f57b666ff747e91626
Jonathan S Vargas <jonathan.vargas01@epn.edu.ec>
2020-07-14 18:16:06 -0500

    variables en typescript

---
+++ 02-Typescript/01-variables.ts
@@ -0,0 +1,19 @@
+let nombre:string = "Jona";
+nombre = "Santiago";
+
+let edad:number = 23;
+let casado:boolean = false;
+let fecha = new Date()
+
+let sueldo:number;
+sueldo = 2.4
+
+let marihuana:any = 2
+marihuana = '2'
+marihuana = true
+marihuana = () =>3
+
+//definir distitos tipos de datos para la misma variable
+
+let edadMultiple: number | string = 2
+edadMultiple = '2'
